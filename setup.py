c3ef6389d04f9cbebd95adb63caf627376196910
Sana Braham <brahamsana@gmail.com>
2023-04-09 08:43:33 +0200

    first commit and added raw data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='It is the wafer project with MLops',
+    author='Sana Braham',
+    license='MIT',
+)
